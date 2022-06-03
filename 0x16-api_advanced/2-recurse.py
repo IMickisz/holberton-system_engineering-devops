5d516aa02890c9dc9ca26b594b1b02045b46abd8
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-06-03 15:52:00 +0200

    added after in the prototype

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,19 +7,20 @@ Module that contains the function recurse
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
-    """
-    Recursive function that queries the Reddit API and returns a
-    list containing the titles of all hot articles for a given subreddit
-    """
+def recurse(subreddit, hot_list=[], after=""):
+    """Recursive function that queries the Reddit API and returns a
+    list containing the titles of all hot articles for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    req = requests.get(url, headers={'User-agent': 'Adventurous-Bed5688'},
+    header = {'User-agent': 'Adventurous-Bed5688'}
+    param = {'after': after}
+    req = requests.get(url, headers=header, params=param,
                        allow_redirects=False)
     if req.status_code > 300:
         return None
-    else:
-        for i in req.json().get('data').get('children'):
-            hot_list.append(i.get('title'))
-    if not req.json().get('data').get('after'):
-        return hot_list
-    return recurse(subreddit, hot_list)
+
+    for i in req.json().get('data').get('children'):
+        hot_list.append(i.get('title'))
+
+    if req.json().get('data').get('after') is not None:
+         return recurse(subreddit, hot_list, after)
+    return hot_list
