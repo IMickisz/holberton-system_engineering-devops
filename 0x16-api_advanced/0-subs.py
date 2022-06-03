936bfdb112c5a189cc48fd02c434a3efb611a0a8
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-06-03 11:00:36 +0200

    added tasks 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module that contains the function number_of_subscribers.
+"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    req = requests.get(url, headers={'User-agent': 'Adventurous-Bed5688'},
+                           allow_redirects=False)
+    if req.status_code > 300:
+        return 0
+    return(req.json().get('data').get('subscribers'))
